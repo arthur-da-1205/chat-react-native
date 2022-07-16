afb6cd44ed890f40eadcfe8ec34a55edf13461a1
arthur-da-1205 <arthur.darwanto@gmail.com>
2022-07-16 23:37:10 +0700

    Slicing Sing In Screen

--- src/components/atoms/LinkButton/index.js
+++ src/components/atoms/LinkButton/index.js
@@ -1,14 +1,22 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const LinkButton = () => {
+const LinkButton = ({label, size, align}) => {
   return (
     <View>
-      <Text>LinkButton</Text>
+      <Text style={styles.label(size, align)}>{label}</Text>
     </View>
   );
 };
 
 export default LinkButton;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  label: (size, align) => ({
+    fontSize: size,
+    color: '#7d8797',
+    fontFamily: 'Nunito-Regular',
+    textDecorationLine: 'underline',
+    textAlign: align,
+  }),
+});
--- src/screens/SignInScreen/index.js
+++ src/screens/SignInScreen/index.js
@@ -1,7 +1,7 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {Logo} from '../../assets';
-import {Button, InputField, LinkButton} from '../../components';
+import {Button, Gap, InputField, LinkButton} from '../../components';
 
 const SignInScreen = () => {
   return (
@@ -9,10 +9,14 @@ const SignInScreen = () => {
       <Logo />
       <Text style={styles.title}>Masuk dan Mulai Konsultasi</Text>
       <InputField label="Email Address" />
+      <Gap height={24} />
       <InputField label="Password" />
-      <LinkButton />
+      <Gap height={10} />
+      <LinkButton label="Forgot My Password" size={12} />
+      <Gap height={40} />
       <Button title="Sign In" />
-      <LinkButton />
+      <Gap height={30} />
+      <LinkButton label="Create New Account" size={16} align="center" />
     </View>
   );
 };
